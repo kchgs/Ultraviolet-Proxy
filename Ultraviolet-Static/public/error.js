c95011dad43f44b80c6477dc7d4d07414916962a
s1871012 <118460956+s1871012@users.noreply.github.com>
2023-12-18 21:08:48 -0600

    Update error.js

--- Ultraviolet-Static/public/error.js
+++ Ultraviolet-Static/public/error.js
@@ -7,7 +7,14 @@ if (location.pathname.startsWith(__uv$config.prefix)) {
   error.textContent = "Click the button (twice, wait for load after 1st click) to start. ";
   registerButton.classList.add("show");
 }
-
+ try {
+    await registerSW();
+    location.reload();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    registerButton.classList.remove("show");
+  }
 registerButton.addEventListener("click", async () => {
   try {
     await registerSW();
